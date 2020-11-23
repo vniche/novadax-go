07a4b521441b351be486de6223a9406dddcf622e
Vinicius Niche Correa <viniciusnichecorrea@gmail.com>
2020-11-23 18:39:20 -0300

    adds GetBalance method

--- account.go
+++ account.go
@@ -25,3 +25,30 @@ func (client *Client) AccountSubs() ([]*SubAccount, error) {
 	_, err = client.do(req, &response)
 	return response.SubAccounts, err
 }
+
+// Balance represents a coin balance
+type Balance struct {
+	Currency  string `json:"currency"`
+	Balance   string `json:"balance"`
+	Hold      string `json:"hold"`
+	Available string `json:"available"`
+	AccountID string `json:"accountId"`
+}
+
+// GetBalance stands for the response structure for account get balance API endpoint
+type GetBalance struct {
+	Code     string     `json:"code"`
+	Balances []*Balance `json:"data"`
+	Message  string     `json:"message"`
+}
+
+// AccountGetBalance returns current account balances
+func (client *Client) AccountGetBalance() ([]*Balance, error) {
+	req, err := client.buildRequest("GET", "/v1/account/getBalance", nil, true)
+	if err != nil {
+		return nil, err
+	}
+	var response GetBalance
+	_, err = client.do(req, &response)
+	return response.Balances, err
+}
